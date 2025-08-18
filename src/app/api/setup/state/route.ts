fd57bfc6da16695e8bee9512c690ad26348f2047
BisonByte <blackshainorg@gmail.com>
2025-08-18 17:49:20 -0400

    feat: add unified setup handlers

--- src/app/api/setup/state/route.ts
+++
@@ -1,8 +0,0 @@
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  const cookie = cookies().get('setup-state')
-  const data = cookie ? JSON.parse(cookie.value) : {}
-  return NextResponse.json(data)
-}
